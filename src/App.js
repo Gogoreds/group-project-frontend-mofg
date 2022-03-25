9e4358b92e8cde2d25dd0aa0d2d1b64b9a911a63
Fredrik Wisborg <fredrik_wisborg@hotmail.com>
2022-03-25 09:36:32 +0100

    Index comment file fix

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 
 function App() {
   return (
-
-    //Let us only implement our components here. Functions and logic can be written in separate .fs files
-  
     <div className="App">
       <h1>Home Page</h1>
       <p>Home page content!</p>
